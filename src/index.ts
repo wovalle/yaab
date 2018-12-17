544c98838bc30d15a607be5d144ba890bb1f38cf
Willy Ovalle <willyovalle16@gmail.com>
2018-12-17 23:10:16 +0100

    chore: removing unused stuff

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as functions from 'firebase-functions';
-import processTelegramUpdates from './functions/processTelegramUpdates';
 import importUsers from './functions/importUsers';
-import fetchMessagesByDate from './functions/fetchMessagesByDate';
 import updateLastMessageBetweenDates from './functions/updateLastMessageBetweenDates';
 import fetchInactiveUsersWithinTimeframe from './functions/fetchInactiveUsersWithinTimeframe';
 import onTelegramUpdate from './functions/onTelegramUpdate';
@@ -22,18 +20,6 @@ const telegramService = new TelegramService(telegramKey, http);
 const url = `https://api.telegram.org/bot${telegramKey}/getupdates`;
 const getDate = () => new Date();
 
-export const processTelegramUpdatesFn = functions.https.onRequest(
-  async (_, res) => {
-    try {
-      const updates = await processTelegramUpdates(db, logger, url);
-      return res.send({ ok: true, updates: updates.length });
-    } catch (error) {
-      logger.error(error);
-      return res.status(500).send({ ok: false });
-    }
-  }
-);
-
 export const importUsersInternalFn = functions.https.onRequest(
   async (req, res) => {
     const { users, groupId } = req.body;
@@ -49,37 +35,6 @@ export const importUsersInternalFn = functions.https.onRequest(
     try {
       await importUsers(db, groupId, users);
       return res.send({ ok: true, usersAdded: users.length });
-    } catch (error) {
-      logger.error(error);
-      return res.status(500).send({ ok: false });
-    }
-  }
-);
-
-export const fetchMessagesByDateFn = functions.https.onRequest(
-  async (req, res) => {
-    const { groupId, from, to } = req.body;
-
-    if (!groupId) {
-      return res.status(400).send('`groupId` parameter is required');
-    }
-
-    if (!from) {
-      return res.status(400).send('invalid parameter `from`');
-    }
-
-    if (!to) {
-      return res.status(400).send('invalid parameter `to`');
-    }
-
-    try {
-      const payload = await fetchMessagesByDate(
-        db,
-        groupId,
-        new Date(from),
-        new Date(to)
-      );
-      return res.send({ ok: true, payload });
     } catch (error) {
       logger.error(error);
       return res.status(500).send({ ok: false });
