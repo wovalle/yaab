d89ad89d5bdc641a4ab6993edbb7553ba0d3cbb0
Willy Ovalle <willyovalle16@gmail.com>
2018-12-24 22:00:17 +0100

    fix: fixing some tests!

--- src/fireorm/fixture.ts
+++ src/fireorm/fixture.ts
@@ -22,20 +22,20 @@ users.push({
   id: 'jen',
   firstName: 'Jen',
   lastName: 'Barber',
-  birthDate: new Date(1978, 3 - 1, 9),
+  birthDate: new Date('1978-03-09'),
 });
 
 users.push({
   id: 'moss',
   firstName: 'Maurice',
   lastName: 'Moss',
-  birthDate: new Date(1977, 6 - 1, 12),
+  birthDate: new Date('1977-06-12'),
 });
 
 users.push({
   id: 'roy',
   firstName: 'Roy',
-  birthDate: new Date(1979, 10 - 1, 9),
+  birthDate: new Date('1979-10-09'),
 });
 
 const colFixture = users.reduce((acc, cur) => {
@@ -43,10 +43,10 @@ const colFixture = users.reduce((acc, cur) => {
   return acc;
 }, {});
 
-export const fixtureData = {
+export const getFixture = () => ({
   __collection__: {
     users: {
       __doc__: colFixture,
     },
   },
-};
+});
