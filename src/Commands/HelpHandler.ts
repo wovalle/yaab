bc7dd1c739ae48e986c264a647e03f34631ea66a
Willy Ovalle <willyovalle16@gmail.com>
2019-02-28 21:47:40 +0100

    feat: adding better help

--- src/Commands/HelpHandler.ts
+++ src/Commands/HelpHandler.ts
@@ -19,13 +19,27 @@ export class HelpHandler
   }
 
   async Handle(payload: ITelegramHandlerPayload) {
-    await this.telegramService.sendChat(
-      payload.plainMessage.chat_id,
-      this.i18n.t('commands.help.successful'),
-      {
-        parse_mode: ParseMode.Markdown,
-        reply_to_message_id: payload.plainMessage.message_id,
-      }
-    );
+    const p = (t: string = '') => `${t}\n`;
+    // prettier-ignore
+    const message =
+      p('**===Comandos===**') +
+      p('/addcrush - Agrega a un usuario del grupo como crush. Esto te permite tirarle Dms anónimos 😏') +
+      p('/mutecrush - Mutea un usuario') +
+      p('/disablecrush - Deshabilita que otras personas puedan agregarte como crush') +
+      p('/enablecrush - Habilita que otras personas puedan agregarte como crush') +
+      p('/listcrush - Lista a todos los crush') +
+      p() +
+      p('**===Admin Commands===**') +
+      p('/lobrechadore {num} - Lista a todas las personas que tienen {num} horas inactivos (sin enviar mensajes) en un grupo. Por defecto {num} es 120 horas.') +
+      p('/thanos {num} - Remueve del grupo a todas las personas que tienen {num} horas inactivos (sin enviar mensajes) en un grupo') +
+      p('/delomio {id} - Agrega al usuario con {id} a la lista de usuarios protegidos (aka no pueden ser removidos del grupo con /thanos). Si es usado como reply a un mensaje, el id del usuario que escribió el mensaje al que se le está dando reply será usado.') +
+      p('/baraja {id} - Remueve al usuario con {id} de la lista de usuarios protegidos (aka no pueden ser removidos del grupo con /thanos). Si es usado como reply a un mensaje, el id del usuario que escribió el mensaje al que se le está dando reply será usado.');
+    p('/lodichoso- Lista a todos los usuarios protegidos');
+
+    await this.telegramService
+      .buildMessage(message)
+      .to(payload.plainMessage.chat_id)
+      .asMarkDown()
+      .send();
   }
 }
